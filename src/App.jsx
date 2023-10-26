683615cf67773ff02935fcd878ef43cc160b5430
Durai Vignesh C <109746446+Crazzy-novus@users.noreply.github.com>
2023-10-26 16:57:29 +0000

    Commit made from VS Code Azure Static Web Apps All updates are made

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Durai Vignesh C",
+  title: "| Google developer student club lead 2023 | Android developer | Android Developer Intern @ The Sparks Foundation | Tech enthusiast |",
+  email: "duraivignesh222_bcs25@mepcoeng.ac.in",
+  gitHub: "https://github.com/Crazzy-novus",
+  instagram: "https://www.instagram.com/crazzy_king_22/t",
+  linkedIn: "https://www.linkedin.com/in/duraivignesh-c/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
