8bf13f8842eecd7f87c5f3aef5ef8ef4fa0d1d78
Durai Vignesh C <109746446+Crazzy-novus@users.noreply.github.com>
2023-11-14 13:53:42 +0000

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -13,29 +13,7 @@ import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
 
 import "./styles.css";
-
-/**
- * This object represents your information. The project is set so that you
- * only need to update these here, and values are passed a properties to the
- * components that need that information.
- *
- * Update the values below with your information.
- *
- * If you don't have one of the social sites listed, leave it as an empty string.
- */
-
-/* hello it just a update*/
-const siteProps = {
-  name: "Durai Vignesh C",
-  title: "| Google developer student club lead 2023 | Android developer | Android Developer Intern @ The Sparks Foundation | Tech enthusiast |",
-  email: "duraivignesh222_bcs25@mepcoeng.ac.in",
-  gitHub: "https://github.com/Crazzy-novus",
-  instagram: "https://www.instagram.com/crazzy_king_22/t",
-  linkedIn: "https://www.linkedin.com/in/duraivignesh-c/",
-  medium: "",
-  twitter: "",
-  youTube: "",
-};
+import { siteProps } from "./siteProps";
 
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
---
+++ src/siteProps.jsx
@@ -0,0 +1,20 @@
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+export const siteProps = {
+  name: "Durai Vignesh C",
+  title: "| Google developer student club lead 2023 | Android developer | Android Developer Intern @ The Sparks Foundation | Tech enthusiast |",
+  email: "duraivignesh222_bcs25@mepcoeng.ac.in",
+  gitHub: "https://github.com/Crazzy-novus",
+  instagram: "https://www.instagram.com/crazzy_king_22/t",
+  linkedIn: "https://www.linkedin.com/in/duraivignesh-c/",
+  medium: "",
+  twitter: "",
+  youTube: "",
+};
